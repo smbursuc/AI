5c64333d87ce8ef2f382e52a10ce0a5660e56121
BursucSerban-Mihai <79265695+smbursuc@users.noreply.github.com>
2022-11-03 21:48:18 +0200

    lab5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 file = open("input.txt")
 lines = [line.rstrip() for line in file]
 
@@ -32,83 +34,76 @@ def get_column_vals(matrix,index):
         vals.append(matrix[i][index])
     return vals
 
-def find_dominant_strategies(matrix,player):
+def find_dominant_strategies_2(matrix,player,**kwargs):
+    max_c = []
+    max_l = []
     if player == 'PlayerA':
-        max_line_vals = []
-        for i in range(len(matrix)):
-            line = matrix[i]
-            maximum = -999999
-            for tuple in line:
-                if int(tuple[0])>maximum:
-                    maximum=int(tuple[0])
-                elif int(tuple[1])>maximum:
-                    maximum=int(tuple[1])
-            max_line_vals.append(maximum)
-        
-        for i in range(len(matrix)):
-            line = matrix[i]
-            good = 1
-            for tuple in line:
-                val1 = int(tuple[0])
-                if val1< min(max_line_vals):
-                    good = 0
-            if good == 1:
-                print("PlayerA are strategie dominanta pe",moves['PlayerA'][i])
-    elif player=='PlayerB':
-        max_col_vals = []
+        max_col_j = []
         for i in range(len(matrix)):
             col = get_column_vals(matrix,i)
-            maximum = -999999
-            for tuple in col:
-                if int(tuple[0])>maximum:
-                    maximum=int(tuple[0])
-                elif int(tuple[1])>maximum:
-                    maximum=int(tuple[1])
-            max_col_vals.append(maximum)
-        
+            max_col = -999999
+            max_j = -1
+            max_i = -1
+            for j in range(len(col)): #j=linie
+                if(int(col[j][0])>max_col):
+                    max_col = int(col[j][0])
+                    max_j = j
+                    max_i = i
+            max_col_j.append(max_j)
+            max_c.append((max_j,max_i))
+            for j in range(len(col)):
+                if int(col[j][0]) == max_col and i!= max_i:
+                    max_l.append((j,i))
+        unique = np.unique(max_col_j)
+        if len(unique) == 1:
+            if(kwargs.get('display')==True):
+                print("PlayerA are strategie dominanta pe",moves['PlayerA'][unique[0]])
+        return max_c
+    elif player == 'PlayerB':
+        max_line_j = []
+        max_line = -99999
         for i in range(len(matrix)):
-            col = get_column_vals(matrix,i)
-            good = 1
-            for tuple in col:
-                val2 = int(tuple[1])
-                if val2 < min(max_col_vals):
-                    good = 0
-            if good == 1:
-                print("PlayerB are strategie dominanta pe",moves['PlayerB'][i])
-
-def find_nash_equilibriums(matrix,first_player):
-    if first_player == 'PlayerA':
-        for i in range(len(matrix)):
-            max_j_index = -1
-            max_line = -999999
-            for j in range(len(matrix)):
-                if int(matrix[i][j][0]) > max_line:
-                    max_line = int(matrix[i][j][0])
-                    max_j_index = j
-        
-            col = get_column_vals(matrix,max_j_index)
-            max_col = -9999999
-            max_i_index = -1
-            for i in range(len(col)):
-                if(int(col[i][1])>max_col):
-                    max_col = int(col[i][1])
-                    max_i_index = i
-            
-            val = matrix[max_i_index][max_j_index]
-            val1 = val[0]
-            val2 = val[1]
-
-
-            if int(val1) == max_line and int(val2) == max_col:
-                print("Exista echilibru Nash la valoarea: (",max_line,",",max_col,")")
-                
-        
-            
+            line = matrix[i]
+            max_line = -99999
+            max_j = -1
+            max_i = -1
+            for j in range(len(line)):
+                if(int(line[j][1])>max_line):
+                    max_line = int(line[j][1])
+                    max_j = j
+                    max_i = i
+            max_line_j.append(max_j)
+            max_l.append((max_i,max_j))
+            for j in range(len(line)):
+                if int(line[j][1]) == max_line and j!= max_j:
+                    max_l.append((i,j))
+        unique = np.unique(max_line_j)
+        if len(unique) == 1:
+            if(kwargs.get('display')==True):
+                print("PlayerB are strategie dominanta pe",moves['PlayerB'][unique[0]])
+        return max_l
 
 
 
-find_dominant_strategies(matrix,'PlayerB')
-find_nash_equilibriums(matrix,'PlayerA')
+def find_nash_equilibriums_2(matrix):
+    setA = set(find_dominant_strategies_2(matrix,'PlayerA',display=False))
+    setB = set(find_dominant_strategies_2(matrix,'PlayerB',display=False))
+    intersection = setA.intersection(setB)
+    for int in intersection:
+        print("Exista echilibru Nash la valoarea: (",int[0],",",int[1],")")
+
+
+
+find_nash_equilibriums_2(matrix)
+
+
+players = ['PlayerA','PlayerB']
+
+for player in players:
+    print("For " + player + ":")
+    find_dominant_strategies_2(matrix,player,display=True)
+
+
 
 
 
